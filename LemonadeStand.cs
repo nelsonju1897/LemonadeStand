a49dcd99642d8abff107789e244c361f661a1890
nelsonju1897 <nelsonju1897@gmail.com>
2019-10-28 14:25:26 -0500

    Weather condition and tempurature are randomly generated. Working on days of the week.

--- LemonadeStand.cs
+++ LemonadeStand.cs
@@ -10,9 +10,20 @@ namespace LemonadeStand
     {
         static void Main(string[] args)
         {
+            Weather w = new Weather();
+            //w.DailyWeatherCondition();
+            //w.DailyTempCondition();
+
+            Game game = new Game();
+            game.RunGame();
+
+        ///////////////////////////////////////////////////////////////////////////////////////
+
             // Display rules
             // Player inputs name
             // Display forcast
+                //Forcast should range from Sunny to Rainy
+                //Forcast should range in tempurature from 55 degrees to 95 degrees
             // Display day
             // Display wallet
             // Display Inventory
@@ -26,12 +37,19 @@ namespace LemonadeStand
             // Display current recipe
                 // Give option to change recipe
                     // While changing recipe
-                            // Change amount of ice cubes
-                            // Change amount of lemons
-                            // Change amount of sugar
-                            // Return home
+                        // Change amount of ice cubes
+                        // Change amount of lemons
+                        // Change amount of sugar
+                        // Return home
             // Give option to start day
                 //When day starts
+                    //Customers go by and buy lemonade determined by random criteria
+                        //Random criteria: Price of lemonade and weather conditions
+                    // Amount of lemons, ice, and sugar will decrease per number of pitchers made
+                    // Number of cups will decrease per cups of lemonade sold
+                    //Pitchers will be determined by number of cups of lemonade can be made out of them
+
+
 
         }
     }
