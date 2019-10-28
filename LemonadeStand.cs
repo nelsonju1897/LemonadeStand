079cea0a04145699854ded7e49f24da33da7cefd
nelsonju1897 <nelsonju1897@gmail.com>
2019-10-28 09:49:44 -0500

    Initial commit. Added current classes. Planning out how the game will run.

---
+++ LemonadeStand.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LemonadeStand
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Display rules
+            // Player inputs name
+            // Display forcast
+            // Display day
+            // Display wallet
+            // Display Inventory
+            // Give option to go to store
+                // While at the store
+                    // Buy cups
+                    // Buy ice cubes
+                    // Buy lemons
+                    // Buy sugar
+                    // return home
+            // Display current recipe
+                // Give option to change recipe
+                    // While changing recipe
+                            // Change amount of ice cubes
+                            // Change amount of lemons
+                            // Change amount of sugar
+                            // Return home
+            // Give option to start day
+                //When day starts
+
+        }
+    }
+}
