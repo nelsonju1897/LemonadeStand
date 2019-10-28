53e83e52694e8da82e266edcf562debc1941d5b1
nelsonju1897 <nelsonju1897@gmail.com>
2019-10-28 16:06:52 -0500

    Created a for loop to go through each day. The days each state what day it is and the weather. Will be working on wallet.

--- LemonadeStand.cs
+++ LemonadeStand.cs
@@ -10,44 +10,48 @@ namespace LemonadeStand
     {
         static void Main(string[] args)
         {
-            Weather w = new Weather();
+            //Day d = new Day();
+            //d.DayOne();
+            //Weather w = new Weather();
             //w.DailyWeatherCondition();
             //w.DailyTempCondition();
-
+            //Game ja = new Game();
+            //ja.DailyActivities();
             Game game = new Game();
             game.RunGame();
 
-        ///////////////////////////////////////////////////////////////////////////////////////
+            ///////////////////////////////////////////////////////////////////////////////////////
 
-            // Display rules
+            // Display rules - Put in actual rules
             // Player inputs name
             // Display forcast
-                //Forcast should range from Sunny to Rainy
-                //Forcast should range in tempurature from 55 degrees to 95 degrees
+            //Forcast should range from Sunny to Rainy
+            //Forcast should range in tempurature from 55 degrees to 95 degrees
             // Display day
+            // Game will start on Monday and work through the week to Sunday
             // Display wallet
             // Display Inventory
             // Give option to go to store
-                // While at the store
-                    // Buy cups
-                    // Buy ice cubes
-                    // Buy lemons
-                    // Buy sugar
-                    // return home
+            // While at the store
+            // Buy cups
+            // Buy ice cubes
+            // Buy lemons
+            // Buy sugar
+            // return home
             // Display current recipe
-                // Give option to change recipe
-                    // While changing recipe
-                        // Change amount of ice cubes
-                        // Change amount of lemons
-                        // Change amount of sugar
-                        // Return home
+            // Give option to change recipe
+            // While changing recipe
+            // Change amount of ice cubes
+            // Change amount of lemons
+            // Change amount of sugar
+            // Return home
             // Give option to start day
-                //When day starts
-                    //Customers go by and buy lemonade determined by random criteria
-                        //Random criteria: Price of lemonade and weather conditions
-                    // Amount of lemons, ice, and sugar will decrease per number of pitchers made
-                    // Number of cups will decrease per cups of lemonade sold
-                    //Pitchers will be determined by number of cups of lemonade can be made out of them
+            //When day starts
+            //Customers go by and buy lemonade determined by random criteria
+            //Random criteria: Price of lemonade and weather conditions
+            // Amount of lemons, ice, and sugar will decrease per number of pitchers made
+            // Number of cups will decrease per cups of lemonade sold
+            //Pitchers will be determined by number of cups of lemonade can be made out of them
 
 
 
