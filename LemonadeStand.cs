4ab8ef4eaa1541a44b0d1b255030d7cda28f6444
nelsonju1897 <nelsonju1897@gmail.com>
2019-10-31 08:39:07 -0500

    Finished the ability to buy items from the store and prevent player from buying items if they do not have enough money. Also added the option to return to the main menu.

--- LemonadeStand.cs
+++ LemonadeStand.cs
@@ -21,19 +21,19 @@ namespace LemonadeStand
             ///////////////////////////////////////////////////////////////////////////////////////
 
             // Display rules - Put in actual rules
-            // Player inputs name
-            // Display forcast
-            //Forcast should range from Sunny to Rainy
-            //Forcast should range in tempurature from 55 degrees to 95 degrees
-            // Display day
-            // Display wallet
-            // Display Inventory
-            // Give option to go to store
-            // While at the store
-            // Buy cups
-            // Buy ice cubes
-            // Buy lemons
-            // Buy sugar
+            // Player inputs name x
+            // Display forcast x
+            //Forcast should range from Sunny to Rainy x
+            //Forcast should range in tempurature from 55 degrees to 95 degrees x
+            // Display day x
+            // Display wallet x
+            // Display Inventory x
+            // Give option to go to store x
+            // While at the store x
+            // Buy cups x
+            // Buy ice cubes x
+            // Buy lemons x
+            // Buy sugar x
             // return home
             // Display current recipe
             // Give option to change recipe
